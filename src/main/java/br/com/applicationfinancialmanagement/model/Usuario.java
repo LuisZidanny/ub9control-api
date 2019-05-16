0b65a2e3f0dfa2e6355dc738256a8ee489cf3453
Luis Zidanny <zidanyy123@gmail.com>
2019-05-16 12:17:25 -0300

    Funciona

--- src/main/java/br/com/applicationfinancialmanagement/model/Usuario.java
+++ src/main/java/br/com/applicationfinancialmanagement/model/Usuario.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -20,7 +18,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Usuario {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 
 	private String nome;
